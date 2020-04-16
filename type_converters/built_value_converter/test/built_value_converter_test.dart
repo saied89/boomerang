542dcd08fb094c6b6dac587478c732f657e4db7e
saied <saiedmomen@gmail.com>
2020-04-16 19:08:07 +0430

    init built value converter

--- type_converters/built_value_converter/test/built_value_converter_test.dart
+++ type_converters/built_value_converter/test/built_value_converter_test.dart
@@ -2,15 +2,15 @@ import 'package:built_value_converter/built_value_converter.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    Awesome awesome;
-
-    setUp(() {
-      awesome = Awesome();
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
+//  group('A group of tests', () {
+//    Awesome awesome;
+//
+//    setUp(() {
+//      awesome = Awesome();
+//    });
+//
+//    test('First Test', () {
+//      expect(awesome.isAwesome, isTrue);
+//    });
+//  });
 }
