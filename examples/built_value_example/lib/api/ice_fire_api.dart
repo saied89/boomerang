e4d06a568cf191f37280c37e71473eeaf2de323e
saied <saiedmomen@gmail.com>
2020-04-22 13:48:54 +0430

    style correction

--- examples/built_value_example/lib/api/ice_fire_api.dart
+++ examples/built_value_example/lib/api/ice_fire_api.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:boomerang/boomerang.dart';
 import 'package:boomerang/src/method.dart';
 import 'package:built_value_converter/built_value_converter.dart';
@@ -8,12 +6,14 @@ import 'package:built_value_example/serializer/serializers.dart';
 import 'package:http/http.dart' as http;
 
 final converter = BuiltValueConverter(serializers);
-final boomerang = Boomerang(converter, http.Client(), 'https://anapioficeandfire.com/api/');
+final boomerang =
+    Boomerang(converter, http.Client(), 'https://anapioficeandfire.com/api/');
 
-Call<Character> getSnow = Call(Get(), url: 'https://anapioficeandfire.com/api/characters/583');
+Call<Character> getSnow =
+    Call(Get(), url: 'https://anapioficeandfire.com/api/characters/583');
 
 //Call<Character> getCharacterByNum(int num) => Call(Get(), url: 'https://anapioficeandfire.com/api/characters/583');
 
-extension Equeue <T> on Call<T> {
+extension Equeue<T> on Call<T> {
   Future<Response<T>> enqueue() => boomerang.dispatch(this);
 }
