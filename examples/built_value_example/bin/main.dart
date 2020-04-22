00505fbe39be7344e6825b4dfa1669ad2979547d
saied <saiedmomen@gmail.com>
2020-04-22 12:23:18 +0430

    fix

--- examples/built_value_example/bin/main.dart
+++ examples/built_value_example/bin/main.dart
@@ -1,6 +1,7 @@
 import 'package:built_value_example/api/ice_fire_api.dart';
 
 void main(List<String> arguments) async {
-  final char = (await getSnow.enqueue()).body;
+  var response = await getSnow.enqueue();
+  final char = response.body;
   print(char.aliases);
 }
