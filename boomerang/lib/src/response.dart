e685f44c8aac43caa609d638b10922d79d0c1399
saied <saiedmomen@gmail.com>
2020-04-22 11:35:40 +0430

    fixes

--- boomerang/lib/src/response.dart
+++ boomerang/lib/src/response.dart
@@ -1,14 +1,8 @@
 import 'package:http/http.dart' as http;
 
-class Response<T> extends http.BaseResponse {
+class Response<T> {
   final T body;
+  final http.Response httpResponse;
 
-  Response(http.BaseResponse httpResponse, this.body)
-      : super(httpResponse.statusCode,
-            contentLength: httpResponse.contentLength,
-            request: httpResponse.request,
-            headers: httpResponse.headers,
-            isRedirect: httpResponse.isRedirect,
-            persistentConnection: httpResponse.persistentConnection,
-            reasonPhrase: httpResponse.reasonPhrase);
+  Response(this.httpResponse, this.body);
 }
--- examples/built_value_example/bin/main.dart
+++ examples/built_value_example/bin/main.dart
@@ -2,6 +2,6 @@ import 'package:built_value_example/api/ice_fire_api.dart';
 import 'package:built_value_example/model/Character.dart';
 
 void main(List<String> arguments) async {
-  Character char = (await getSnow.enqueue()).body;
+  final char = (await getSnow.enqueue()).body;
   print(char.aliases);
 }
