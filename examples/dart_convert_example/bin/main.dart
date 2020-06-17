b353d689346ac36c797fde675638381f1f5d0b21
saied <saiedmomen@gmail.com>
2020-06-17 21:31:54 +0430

    fix example

--- examples/dart_convert_example/bin/main.dart
+++ examples/dart_convert_example/bin/main.dart
@@ -2,6 +2,8 @@ import 'package:dart_convert_example/api/ice_fire_api.dart';
 
 void main(List<String> arguments) async {
   final res = await getSnow.enqueue();
+  final res1 = await getCharacters.enqueue();
   final bodyMap = res.body;
   print(bodyMap['aliases']);
+  print(res1.body.length);
 }
